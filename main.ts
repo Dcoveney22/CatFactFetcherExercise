1647620ae7f430f7d6ac7728ce23349126ac2892
David Coveney <david.coveney01@gmail.com>
2025-01-02 09:55:26 +0000

    First start

---
+++ main.ts
@@ -0,0 +1,32 @@
+export class CatFact {
+  url = "https://catfact.ninja/";
+
+  async findAPI(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    // return data;
+    // console.log(data);
+      printFact(data) {
+    const button = document.getElementById("fetchButton") as HTMLButtonElement;
+    const fact = document.getElementById("factDisplay") as HTMLBodyElement;
+    button?.addEventListener("click", function () {
+      button.onclick = () => {
+        fact;
+      };
+
+      // console.log(fact);
+    })};
+  }
+let catFact = new CatFact()
+  await catFact.findAPI("https://catfact.ninja/")
+}
+
+
+// let image = document.getElementById("image1") as HTMLImageElement;
+// const button = document.getElementById("fetchButton") as HTMLButtonElement;
+// button.addEventListener("click", function () {
+//   button.onclick = () => {
+//     image.src =
+//       "https://cdn.shopify.com/s/files/1/0106/1539/0266/files/Collection_Banner_b9a9c9fb-6540-4032-bfaa-5bb6ef163392.jpg?v=1642797548";
+//   };
+// });
